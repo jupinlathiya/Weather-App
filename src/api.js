5d1cffd0a4edf6ddcda6c6b3d2954dc45049e5a4
jupinlathiya <jupinlathiya008@gmail.com>
2022-08-04 21:34:53 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "", //enter your Geodb-cities key
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+//https://rapidapi.com/wirefreethought/api/geodb-cities/
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
